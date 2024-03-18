6671189224fcc52e9bd588e147b03cca27f6c9b8
Yael Tzur <116788451+yaelt1@users.noreply.github.com>
2024-03-18 16:34:47 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -53,6 +53,14 @@ def convert_tokens_to_one_hot(tokens):
         return functional.one_hot(tokens, num_classes=len(AMINO)).float()
 
 def find_max_length_sequence(series):
+    """Find the maximum length of peptide sequences in a series.
+
+    Args:
+        series: Series containing peptide sequences.
+
+    Returns:
+        int: Maximum length of peptide sequences.
+    """
     max_length = 0
     for sequence in series:
         length = len(sequence)
@@ -83,7 +91,13 @@ def pearson_correlation_coefficient(y_true, y_pred):
 
     pearson_r = K.mean(y_true * y_pred)
     return pearson_r
+
 def fnn_model():
+    """Builds a feedforward neural network (FNN) model for peptide binding prediction.
+
+    Returns:
+        keras.Model: FNN model.
+    """
     N=5
     model = Sequential()
     model.add(Input([16,17]))
@@ -98,6 +112,11 @@ def fnn_model():
     return model
 
 def cnn_model():
+    """Builds a convolutional neural network (CNN) model for peptide binding prediction.
+
+    Returns:
+        keras.Model: CNN model.
+    """
     model = tf.keras.Sequential([
         tf.keras.layers.Conv1D(32, kernel_size=3, activation='relu'),
         tf.keras.layers.MaxPool1D(pool_size=2, strides=2),
@@ -116,6 +135,16 @@ def cnn_model():
     return model
     
 def fit_model(model_name, one_hot_sequences, labels):   
+    """Fits the specified machine learning model on the data.
+
+    Args:
+        model_name (str): Name of the model ('fnn' or 'cnn').
+        one_hot_sequences (array-like): One-hot encoded peptide sequences.
+        labels (array-like): Labels for the sequences.
+
+    Returns:
+        keras.Model: Trained machine learning model.
+    """
     kf = KFold(n_splits=5, shuffle=True)
     for fold, (train_index, test_index) in enumerate(kf.split(one_hot_sequences)):
         if model_name.lower()=="fnn":
@@ -132,6 +161,15 @@ def fit_model(model_name, one_hot_sequences, labels):
 
 
 def get_model_data(filepath,max_length):
+    """Reads data from a CSV file and preprocesses it for model training.
+
+    Args:
+        filepath (str): Path to the CSV file containing data.
+        max_length (int): Maximum length of peptide sequences.
+
+    Returns:
+        tuple: Tuple containing one-hot encoded sequences and corresponding labels.
+    """
     df = pd.read_csv(filepath)
     sequences = df.iloc[:, 0]
     scores =  df.iloc[:, 1]
@@ -147,12 +185,32 @@ def get_model_data(filepath,max_length):
     
     
 def fit_on_data(train_filepath, max_length, model="fnn"):
+    """Fits the specified model on the training data.
+
+    Args:
+        train_filepath (str): Path to the CSV file containing training data.
+        max_length (int): Maximum length of peptide sequences.
+        model (str, optional): Name of the model ('fnn' or 'cnn'). Defaults to 'fnn'.
+
+    Returns:
+        keras.Model: Trained machine learning model.
+    """
     K_backend.clear_session()
     train_one_hot_sequences, train_scores = get_model_data(train_filepath, max_length)
     model = fit_model(model, train_one_hot_sequences, train_scores)
     return model
 
 def predict_on_data(model, predict_filepath, max_length):
+    """Makes predictions using the trained model on the test data.
+
+    Args:
+        model (keras.Model): Trained machine learning model.
+        predict_filepath (str): Path to the CSV file containing test data.
+        max_length (int): Maximum length of peptide sequences.
+
+    Returns:
+        tuple: Tuple containing Pearson correlation coefficient and predicted values.
+    """
     test_one_hot_sequences, test_scores = get_model_data(predict_filepath, max_length)
     predictions = model.predict(test_one_hot_sequences, batch_size=None, verbose="auto", steps=None, callbacks=None)
     np_scores = test_scores.to_numpy()
@@ -165,6 +223,13 @@ def predict_on_data(model, predict_filepath, max_length):
     
         
 def train_pred_all_pairs(model_name, proteins_dir, max_length=16):
+    """Trains the specified model on all pairs of protein files in the directory and saves results.
+
+    Args:
+        model_name (str): Name of the model ('fnn' or 'cnn').
+        proteins_dir (str): Directory containing protein CSV files.
+        max_length (int, optional): Maximum length of peptide sequences. Defaults to 16.
+    """
     files = [file for file in os.listdir(proteins_dir)]
     df = pd.DataFrame(columns= files)
     for train_file in files:
@@ -183,6 +248,14 @@ def train_pred_all_pairs(model_name, proteins_dir, max_length=16):
 
          
 def scatter_test_pred(fit_filepath, predict_file_path, model_name,max_length=16): 
+    """Plots a scatter plot of true vs predicted scores for a specified model.
+
+    Args:
+        fit_filepath (str): Path to the CSV file containing training data.
+        predict_file_path (str): Path to the CSV file containing test data.
+        model_name (str): Name of the model ('fnn' or 'cnn').
+        max_length (int, optional): Maximum length of peptide sequences. Defaults to 16.
+    """
     test_one_hot_sequences, test_scores = get_model_data(predict_file_path, max_length)
     model = fit_on_data(fit_filepath, max_length, model_name)
     pearson_coef_pred,predictions = predict_on_data(model, predict_file_path, max_length)
@@ -196,7 +269,22 @@ def scatter_test_pred(fit_filepath, predict_file_path, model_name,max_length=16)
     plt.grid(True)
     plt.show()
     
+def heat_map(filepath):
+      """Generate a heatmap of Pearson correlation coefficients from a CSV file.
 
+    Args:
+        filepath (str): Path to the CSV file containing Pearson correlation coefficients.
+
+    Returns:
+        None
+    """
+    df = pd.read_csv(filepath, index_col=0)
+    df_sorted_rows = df.sort_index()
+    df_sorted_both = df_sorted_rows[sorted(df_sorted_rows.columns)]
+    plt.figure(figsize=(10, 8))
+    sns.heatmap(df_sorted_both, annot=True, cmap='coolwarm', fmt=".2f", linewidths=.5)
+    plt.title('Pearson Correlation Coefficients CNN')
+    plt.show()
     
     
 if __name__=="__main__":
